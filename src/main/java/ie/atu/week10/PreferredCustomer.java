c45ab02ab42a36b05645cd73789a2908003e2220
Ethan <G00467054@atu.ie>
2025-11-25 21:46:40 +0000

    Step 6 of project

--- src/main/java/ie/atu/week10/PreferredCustomer.java
+++ src/main/java/ie/atu/week10/PreferredCustomer.java
@@ -8,22 +8,21 @@ public class PreferredCustomer extends Customer {
         loyaltyPoints = 0;
         discountLevel = 0;
     }
-public void addLoyaltyPoints(int Points) {
+    public void addLoyaltyPoints(int Points) {
         this.loyaltyPoints += Points;
         updateDiscountLevel();
-}
+    }
     public PreferredCustomer(String name, String address, String phoneNumber, int customerNumber, boolean mailingList, int loyaltyPoints, double discountLevel) {
         super(name, address, phoneNumber, customerNumber, mailingList);
         this.loyaltyPoints = loyaltyPoints;
         this.discountLevel = discountLevel;
     }
     private void updateDiscountLevel() {// set discountLevel based on loyaltyPoints
-
         if (loyaltyPoints < 500) discountLevel = 0;
         if (loyaltyPoints >= 500 && loyaltyPoints <= 999) discountLevel = 0.05;
-     if (loyaltyPoints >= 1000 && loyaltyPoints <= 1499) discountLevel = 0.06;
-     if (loyaltyPoints >= 1500 && loyaltyPoints <= 1999) discountLevel = 0.07;
-     if (loyaltyPoints >= 2000) discountLevel = 0.1;
+        if (loyaltyPoints >= 1000 && loyaltyPoints <= 1499) discountLevel = 0.06;
+        if (loyaltyPoints >= 1500 && loyaltyPoints <= 1999) discountLevel = 0.07;
+        if (loyaltyPoints >= 2000) discountLevel = 0.1;
     }
 
     public int getLoyaltyPoints() {
@@ -39,9 +38,9 @@ public void addLoyaltyPoints(int Points) {
         return discountLevel;
     }
 
-    public void setDiscountLevel(double discountLevel) {
+    /*public void setDiscountLevel(double discountLevel) {
         this.discountLevel = discountLevel;
-    }
+    }*/
 
     @Override
     public String toString() {
